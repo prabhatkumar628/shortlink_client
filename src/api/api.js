eae1cf2384662b3b07f59e7d2f24e3320aa5d5ff
Prabha Kumar <kprabhat628@gmail.com>
2026-01-12 20:16:22 +0530

    fixed axios client base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const URL = import.meta.env.VITE_BACKEND_URL
+  ? `${import.meta.env.VITE_BACKEND_URL}/api/v1`
+  : "/api/v1";
+
 export const api = axios.create({
-  baseURL: '/api/v1/',
+  baseURL: URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
